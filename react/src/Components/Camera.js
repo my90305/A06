9655061a4a1b0af2dc60a5a0ee41b22185e439c1
A8222838 <A8222838@g.pccu.edu.tw>
2022-05-24 21:18:18 +0800

    react.js

---
+++ react/src/Components/Camera.js
@@ -0,0 +1,35 @@
+import React from 'react';
+
+
+
+const ChangeArduinoYoutube=()=> {
+    document.getElementById("Screen1").src="https://www.youtube.com/embed/N4r9tQGmAaY";
+    document.getElementById("Screen2").src="https://www.youtube.com/embed/aDKadRU3T80";
+}
+
+const ChangeCameraScreen=()=> {
+    document.getElementById("Screen1").src="http://192.168.31.57";
+    document.getElementById("Screen2").src="http://192.168.31.57:81/stream";
+}
+
+
+
+
+function Camera() {
+    
+
+    return(
+        <div>
+            
+            
+            <iframe title="影像遷入" id="Screen1" width="560" height="315" src="http://192.168.31.57" name="iframe_a" frameborder="0"  allowfullscreen></iframe>
+            <iframe title="影像入" id="Screen2" width="560" height="315" src="http://192.168.31.57:81/stream" name="iframe_b" frameborder="0"  allowfullscreen></iframe>
+            <br/>
+            <button onClick={ChangeArduinoYoutube}>機關試做</button>
+            <button onClick={ChangeCameraScreen}>畫面串流</button>
+            
+        </div>
+    );
+} 
+
+export default Camera;
