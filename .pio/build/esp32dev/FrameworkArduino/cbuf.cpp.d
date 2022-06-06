d84e9e4e7246173453ee531702638e4bfe2f3054
andy <andychung121@gmail.com>
2022-06-06 16:36:26 +0800

    RFID

--- .pio/build/esp32dev/FrameworkArduino/cbuf.cpp.d
+++
@@ -1,3 +0,0 @@
-.pio/build/esp32dev/FrameworkArduino/cbuf.cpp.o: \
- C:/Users/user/.platformio/packages/framework-arduinoespressif32/cores/esp32/cbuf.cpp \
- C:/Users/user/.platformio/packages/framework-arduinoespressif32/cores/esp32/cbuf.h
