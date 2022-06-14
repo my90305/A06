fa5d8054578ee50f5a468d30283ef427f699ce71
tasiXiangYan <allen2356@gmail.com>
2022-06-14 13:44:23 +0800

    fix some error

--- next/pages/api/verifyRFID/[...cardNumber].js
+++ next/pages/api/verifyRFID/[...cardNumber].js
@@ -1,29 +1,30 @@
 import { RFID } from '../../../data/RFIDdata'
-import { useRouter } from "next/router"
 
 export default function handler (req, res){
-    const router = useRouter()
-    const { cardNumber=[] } = router.query
+    const cardNumber = req.query.cardNumber
+    
     console.log(cardNumber)
-
+    res.status(200).json(cardNumber)
+    
+    
     if (cardNumber.length === 2) {
         switch (cardNumber[0]){
             case 1:
-                if (cardNumber[1] == RFID[0]){ return <h1>correct!!!</h1> }
-                else return <h1>So Sorry~~ Not This Card~</h1>
-            
+                if (cardNumber[1] == RFID[0]) {
+                    res.status(200).json({response:'Correct!'})
+                }
+                else res.status(201).json({response:'Not this Card'})
             case 2:
-                if (cardNumber[1] == RFID[1]){ return <h1>correct!!!</h1> }
-                else return <h1>So Sorry~~ Not This Card~</h1>
-
+                if (cardNumber[1] == RFID[1]) {
+                    res.status(200).json({response:'Correct!'})
+                }
+                else res.status(201).json({response:'Not this Card'})
             case 3:
-                if (cardNumber[1] == RFID[2]){ return <h1>correct!!!</h1> }
-                else return <h1>So Sorry~~ Not This Card~</h1>
+                if (cardNumber[1] == RFID[2]) {
+                    res.status(200).json({response:'Correct!'})
+                }
+                else res.status(201).json({response:'Not this Card'})
         }
     }
-
-/*
-    const { cardNumber } = req.query
-    res.end(`GET: ${cardNumber}`)
-*/
+    
 }
