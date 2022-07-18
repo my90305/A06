44e440837d50aaa15347d5454d1462ca641b7417
hc0121 <55742286+hc0121@users.noreply.github.com>
2022-07-18 14:17:19 +0800

    remove node_modules

--- next/pages/_app.js
+++ next/pages/_app.js
@@ -2,6 +2,7 @@ import '../global.css'
 
 export default function MyApp({ Component, pageProps }) {
   // Use the layout defined at the page level, if available
+  console.log(process.env.ELEVATOR_IP);
   const getLayout = Component.getLayout || ((page) => page)
 
   return getLayout(<Component {...pageProps} />)
