097d8fd0555c20c4f3d2576c47bd373b114fbd9a
hc0121 <55742286+hc0121@users.noreply.github.com>
2022-07-28 15:24:11 +0800

    Photo_Interrupter

--- IoT/Photo_Interrupter/Photo_Interrupter.ino
+++ IoT/Photo_Interrupter/Photo_Interrupter.ino
@@ -1,19 +1,19 @@
 int Led = 13; // define LED pin
-int HW487 = 14; // define photo interrupter signal pin
+int P_I = 14; // define photo interrupter signal pin
 int val; //define a numeric variable
 int val1;
 
 void setup()
 {
 pinMode(Led, OUTPUT); // LED pin as output
-pinMode(HW487, INPUT); //photo interrupter pin as input
+pinMode(P_I, INPUT); //photo interrupter pin as input
 Serial.begin(115200);
 }
 
 void loop()
 {
-val=digitalRead(HW487); //read the value of the sensor
-val1 = analogRead(HW487);
+val=digitalRead(P_I); //read the value of the sensor
+val1 = analogRead(P_I);
 if(val == HIGH) // turn on LED when sensor is blocked
 {
 digitalWrite(Led,HIGH);
