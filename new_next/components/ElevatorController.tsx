4ea114d810599b035ac7d4483c76a8f1cd662f00
Peter <peter.t@mydailylive.com>
2022-08-04 16:42:28 +0800

    update

--- new_next/components/ElevatorController.tsx
+++ new_next/components/ElevatorController.tsx
@@ -1,5 +1,5 @@
 import iotApi from 'api/iot';
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import styles from './elevatorController.module.css'
 
 enum VIDEOS_TYPE {
@@ -13,57 +13,40 @@ const screenList = {
   ]
 }
 
-enum DOOR_TYPE {
-  DOOR_OPEN,
-  DOOR_CLOSE,
-}
-
 export default function ElevatorController() {
   const [videoType, setVideoType] = useState(VIDEOS_TYPE.ESP32_LIVE);
   const [screenListSrc, setScreenSrc] = useState(screenList.esp32LiveSrc);
   const [isRealWorldDoorOpen, setIsRealWorldDoorOpen] = useState(false);
   const [isSpiritWorldDoorOpen, setIsSpiritWorldDoorOpen] = useState(false);
 
+  const onRealWorldDoorClick = async () => {
+    const newIsOpen = !isRealWorldDoorOpen
+    setIsRealWorldDoorOpen(newIsOpen)
+    await iotApi.realWrold.door(newIsOpen)
+  }
+
+  const onSpiritWorldDoorClick = async () => {
+    const newIsOpen = !isSpiritWorldDoorOpen
+    setIsSpiritWorldDoorOpen(newIsOpen)
+    await iotApi.spiritWorld.door(newIsOpen)
+  }
+
   return (
     <div>
       {screenListSrc.map(src =>
         <iframe key={src} title={videoType.toString()} width="560" height="315"
-          src={src} frameBorder="0" allowFullScreen />
-      )}
+          src={src} frameBorder="0" allowFullScreen />)}
       <br />
 
       <button className={styles.button}
-        disabled={isRealWorldDoorOpen}
-        onClick={async () => {
-          setIsRealWorldDoorOpen(true)
-          await iotApi.realWrold.door(true)
-        }}>
-        陽間開門
-      </button>
-      <button className={styles.button}
-        disabled={!isRealWorldDoorOpen}
-        onClick={async () => {
-          setIsRealWorldDoorOpen(false)
-          await iotApi.realWrold.door(false)
-        }}>
-        陽間關門
+        onClick={onRealWorldDoorClick}>
+        陽間{isRealWorldDoorOpen ? '開門' : '關門'}
       </button>
       <br />
+
       <button className={styles.button}
-        disabled={isSpiritWorldDoorOpen}
-        onClick={async () => {
-          setIsSpiritWorldDoorOpen(true)
-          await iotApi.realWrold.door(true)
-        }}>
-        陰間開門
-      </button>
-      <button className={styles.button}
-        disabled={!isSpiritWorldDoorOpen}
-        onClick={async () => {
-          setIsSpiritWorldDoorOpen(false)
-          await iotApi.realWrold.door(false)
-        }}>
-        陰間關門
+        onClick={onSpiritWorldDoorClick}>
+        陰間{isSpiritWorldDoorOpen ? '開門' : '關門'}
       </button>
     </div>
   );
