e3e7cd9bcb7b50d6cd86d96d24c647b5f40de2f2
A8222838 <A8222838@g.pccu.edu.tw>
2022-08-04 11:27:13 +0800

    next電梯控制按鈕fetch api

--- new_next/components/ElevatorController.tsx
+++ new_next/components/ElevatorController.tsx
@@ -1,7 +1,9 @@
 import { useEffect, useState } from 'react';
 import styles from './elevatorController.module.css'
 
-const apiDoor = 'http://${process.env.DOOR_IP}';
+const apiDoor1 = `http://${process.env.REAL_WORLD_DOOR_IP}`;
+const apiDoor2 = `http://${process.env.SPIRIT_WORLD_DOOR_IP}`;
+
 
 enum VIDEOS_TYPE {
   ESP32_LIVE,
@@ -19,14 +21,26 @@ enum DOOR_TYPE {
   DOOR_CLOSE,
 }
 
-export async function DoorControllOpen(){
-  const response = await fetch(`${apiDoor}/RELAY=ON`,{
+export function RealWorldDoorControllOpen(){
+  fetch(`${apiDoor1}/DOOR=OPEN`,{
     method: 'GET',
   })
 }
 
-export async function DoorControllClose(){
-  const response = await fetch(`${apiDoor}/RELAY=OFF`,{
+export function RealWorldDoorControllClose(){
+  fetch(`${apiDoor1}/DOOR=CLOSE`,{
+    method: 'GET',
+  })
+}
+
+export function SpiritWorldDoorControllOpen(){
+  fetch(`${apiDoor2}/DOOR=OPEN`,{
+    method: 'GET',
+  })
+}
+
+export function SpiritWorldDoorControllClose(){
+  fetch(`${apiDoor2}/DOOR=CLOSE`,{
     method: 'GET',
   })
 }
@@ -34,19 +48,32 @@ export async function DoorControllClose(){
 export default function ElevatorController() {
   const [videoType, setVideoType] = useState(VIDEOS_TYPE. ESP32_LIVE);
   const [screenListSrc, setScreenSrc] = useState(screenList.esp32LiveSrc);
-  const [doorType, setDoorType] = useState(DOOR_TYPE. DOOR_CLOSE);
+  const [door1Type, setDoor1Type] = useState(DOOR_TYPE. DOOR_CLOSE);
+  const [door2Type, setDoor2Type] = useState(DOOR_TYPE. DOOR_CLOSE);
 
   useEffect(() => {
-    switch (doorType) {
+    switch (door1Type) {
       case DOOR_TYPE.DOOR_OPEN:
       default:
-        DoorControllOpen();
+        RealWorldDoorControllOpen();
         break;
       case DOOR_TYPE.DOOR_CLOSE:
-        DoorControllClose();
+        RealWorldDoorControllClose();
         break;
     }
-  }, [doorType]);
+  }, [door1Type]);
+
+  useEffect(() => {
+    switch (door2Type) {
+      case DOOR_TYPE.DOOR_OPEN:
+      default:
+        SpiritWorldDoorControllOpen();
+        break;
+      case DOOR_TYPE.DOOR_CLOSE:
+        SpiritWorldDoorControllClose();
+        break;
+    }
+  }, [door2Type]);
 
   return (
     <div>
@@ -58,16 +85,26 @@ export default function ElevatorController() {
       <br />
       
       <button className={styles.button}
-        disabled={doorType === DOOR_TYPE.DOOR_OPEN}
-        onClick={() => setDoorType(DOOR_TYPE.DOOR_OPEN)}>
-        開門
+        disabled={door1Type === DOOR_TYPE.DOOR_OPEN}
+        onClick={() => setDoor1Type(DOOR_TYPE.DOOR_OPEN)}>
+        陽間開門
       </button>
       <button className={styles.button}
-        disabled={doorType === DOOR_TYPE.DOOR_CLOSE}
-        onClick={() => setDoorType(DOOR_TYPE.DOOR_CLOSE)}>
-        關門
+        disabled={door1Type === DOOR_TYPE.DOOR_CLOSE}
+        onClick={() => setDoor1Type(DOOR_TYPE.DOOR_CLOSE)}>
+        陽間關門
+      </button>
+      <br/>
+      <button className={styles.button}
+        disabled={door2Type === DOOR_TYPE.DOOR_OPEN}
+        onClick={() => setDoor2Type(DOOR_TYPE.DOOR_OPEN)}>
+        陰間開門
+      </button>
+      <button className={styles.button}
+        disabled={door2Type === DOOR_TYPE.DOOR_CLOSE}
+        onClick={() => setDoor2Type(DOOR_TYPE.DOOR_CLOSE)}>
+        陰間關門
       </button>
     </div>
   );
 }
-
