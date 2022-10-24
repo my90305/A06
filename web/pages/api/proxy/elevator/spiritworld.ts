e352870988a343ff46297b02292a47f30ad13e40
tsaiXiangYan <allen2356@gmail.com>
2022-10-24 10:48:52 +0800

    陰間電梯門proxy

---
+++ web/pages/api/proxy/elevator/spiritworld.ts
@@ -0,0 +1,10 @@
+import iotApi from 'api/iot';
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+export async function handler (req: NextApiRequest, res: NextApiResponse) {
+  const result = await iotApi.spiritWorld.door(false)
+  console.log (result)
+  res.status(200).json({response: 'test success'})
+}
+ 
+export default handler
