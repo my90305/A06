7b94ff10c0db4a5d1bb84011a90884e11a03c579
A8222838 <A8222838@g.pccu.edu.tw>
2022-10-25 16:37:47 +0800

    proxy sever

--- web/api/iot/realWorld.tsx
+++ web/api/iot/realWorld.tsx
@@ -1,9 +1,15 @@
+import { NextApiRequest, NextApiResponse } from "next";
+
 const realWorldDoorIp = `http://${process.env.REAL_WORLD_DOOR_IP}`;
 
 const door = async (isOpen: boolean) => {
     await fetch(`api/elevator/realWorld/${isOpen ? 'doorIsOpen' : 'doorIsClose'}`, {
     method: 'GET',
   })
+}
+
+export async function handler (req: NextApiRequest, res: NextApiResponse) {
+  res.status(200).json({response: 'test success'})
 }
 
 const realWorld = {
--- web/api/iot/spiritWorld.tsx
+++ web/api/iot/spiritWorld.tsx
@@ -1,9 +1,15 @@
+import { NextApiRequest, NextApiResponse } from "next";
+
 const spiritWorldDoorIp = `http://${process.env.SPIRIT_WORLD_DOOR_HOST}`;
 
 const door = async (isOpen: boolean) => {
     await fetch(`api/elevator/spiritWorld/${isOpen ? 'doorIsOpen' : 'doorIsClose'}`, {
     method: 'GET',
   })
+}
+
+export async function handler (req: NextApiRequest, res: NextApiResponse) {
+  res.status(200).json({response: 'test success'})
 }
 
 const spiritWorld = {
