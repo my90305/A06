492bdaedda117c30be0e7af8f005e42d6d046bc7
A8222838 <A8222838@g.pccu.edu.tw>
2022-12-11 21:45:24 +0800

    reset checkArray front

--- web/components/PrismaClear.tsx
+++ web/components/PrismaClear.tsx
@@ -1,9 +1,14 @@
 import database from 'api/prisma';
+import checkArray from 'api/gossiping';
 import styles from './elevatorController.module.css'
 
 export default function PrismaClear() {
     const onPrismaClearClick = async () => {
       await database.init.completeStatus
+    }
+
+    const onResetCheckArrayClick = async () => {
+      await checkArray.init.checkArrayStatus
     }
   
     return (
@@ -11,6 +16,11 @@ export default function PrismaClear() {
         <button className={styles.button}
           onClick={onPrismaClearClick}>
           清空資料庫
+        </button>
+        <br/>
+        <button className={styles.button}
+          onClick={onResetCheckArrayClick}>
+          機關重製
         </button>
       </div>
     );
